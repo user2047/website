c6b1b7477b36a815c844e76d53c901c0f793507e
user2047 <cameron.senna@gmail.com>
2020-06-20 20:39:34 -0400

    first update in a long while

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ let html;
 // Init App
 const app = express();
 app.engine("html", es6Renderer);
-app.set("views", __dirname + "/web/html");
+app.set("views", __dirname + "/resources/html");
 app.set("view engine", "html");
 
 
@@ -30,32 +30,32 @@ const logger = function(req, res, next) {
 
 
 // Public Access
-app.use(express.static('web'));
+app.use(express.static('resources'));
 app.use(logger);
 // Home Route
 
 app.get("/", function(req, res) {
-  res.render("index", {locals: {title: "Home", main: "This is the home page"}})
+  res.render("index", {locals: {title: "Home", main: "This is the home page",content:"This is the main content"}})
 });
 
 app.get("/about", function(req, res) {
-  res.render("index", {locals: {title: "About",main: "This is the about page"}})
+  res.render("index", {locals: {title: "About",main: "About",content:"I am a computer science student who has a passion for linux, web development, and cryptography. I am excited about the opportunity that blockchain and cryptocurrencies can bring to our society, regardless of their usage as a payment network"}})
 });
 
 app.get("/services", function(req, res) {
-  res.render("index", {locals: {title: "Services",main: "This is the services page"}})
+  res.render("index", {locals: {title: "Services",main: "This is the services page",content:"This is the main content"}})
 });
 
 app.get("/contact", function(req, res) {
-  res.render("index", {locals: {title: "Contact",main: "This is the contact page"}})
+  res.render("index", {locals: {title: "Contact",main: "This is the contact page",content:"This is the main content"}})
 });
 
 app.get("/resume", function(req, res) {
-  res.render("index", {locals: {title: "Resume",main: "This is the resume page"}})
+  res.render("index", {locals: {title: "Resume",main: "This is the resume page",content:"This is the main content"}})
 });
 
 app.get("/donate", function(req, res) {
-  res.render("index", {locals: {title: "Donate",main: "This is the donate page"}})
+  res.render("index", {locals: {title: "Donate",main: "This is the donate page",content:"This is the main content"}})
 });
 
 // Start Server
